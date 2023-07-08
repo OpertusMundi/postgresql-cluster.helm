7134165bc85daa2f91fe759ebbf370dc54268c8d
Michail Alexakis <alexakis@athenarc.gr>
2023-07-08 15:27:37 +0300

    [pgpool] Fix script checking status of primary node

--- files/scripts/check-status-of-primary-node.sh
+++ files/scripts/check-status-of-primary-node.sh
@@ -8,14 +8,14 @@ n=$(pcp_node_count ${pcp_opts})
 test $n -gt "0"
 
 # find primary node (for streaming replication) and check status is not down
-# see docs on fields at https://www.pgpool.net/docs/41/en/html/pcp-node-info.html
+# see docs on fields at https://www.pgpool.net/docs/44/en/html/pcp-node-info.html
 for i in $(seq 0 $((n - 1))); do
     # note: mimic Bash arrays by using positional parameters
     # https://unix.stackexchange.com/questions/384614/how-to-port-to-bash-style-arrays-to-ash
     set -- $(pcp_node_info ${pcp_opts} ${i})
     hostname=${1}
-    status_text=${5}
-    backend_role=${6}
+    status_text=${6}
+    backend_role=${7}
     case "${backend_role}" in
       primary)
         if [[ "${status_text}" == 'up' || "${status_text}" == 'waiting' ]]; then
